93f524c3d6d8270e87e61c8fe9077a5bcb23f7f3
JatinThakur-797 <jt6285638@gmail.com>
2026-04-05 12:48:27 +0530

    Implement CRUD operations endpoints in AdminController

--- src/main/java/com/auth/controller/AdminController.java
+++ src/main/java/com/auth/controller/AdminController.java
@@ -1,4 +1,52 @@
 package com.auth.controller;
 
+import com.auth.dto.CreateUserRequest;
+import com.auth.dto.UpdateUserRequest;
+import com.auth.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
+
+    private final AdminService adminService;
+
+    // Create User
+    @PostMapping("/users")
+    public ResponseEntity<?> createUser(@RequestBody CreateUserRequest request) {
+        return ResponseEntity.ok(adminService.createUser(request));
+    }
+
+    // Get All User
+    @GetMapping("/users")
+    public ResponseEntity<?> getAllUsers() {
+        return ResponseEntity.ok(adminService.getAllUsers());
+    }
+
+    // Update User
+    @PutMapping("/users/{id}")
+    public ResponseEntity<?> updateUser(@PathVariable UUID id,
+                                        @RequestBody UpdateUserRequest request) {
+        return ResponseEntity.ok(adminService.updateUser(id, request));
+    }
+
+    // Toggle Status
+    @PatchMapping("/users/{id}/status")
+    public ResponseEntity<?> toggleStatus(@PathVariable UUID id) {
+        return ResponseEntity.ok(adminService.toggleUserStatus(id));
+    }
+
+    // Delete User
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable UUID id) {
+        adminService.deleteUser(id);
+        return ResponseEntity.ok("User deleted successfully");
+    }
 }
